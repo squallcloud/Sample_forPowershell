d005b2862dc033543b912c9d9e5282beee8e3c72
squallcloud <squallcloudzidane@gmail.com>
2019-01-11 00:40:28 +0900

    コミット

---
+++ sample.ps1
@@ -0,0 +1,8 @@
+﻿# https://win.just4fun.biz/?PowerShell/%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E5%86%85%E3%81%AE%E6%96%87%E5%AD%97%E5%88%97%E3%82%92%E7%BD%AE%E6%8F%9B%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95%E3%83%BBreplace%2C%20creplace
+
+$foo = ".\sample.txt"
+# $hoge = Get-Content -Encoding UTF8 $foo | foreach {$_ -replace "hoge", "foo"}
+
+# $hoge | Out-File -Encoding UTF8 $foo
+
+(Get-Content $foo | % {$_ -replace "hoge", "foo"}) | Out-File -Encoding default $foo
---
+++ sample1.ps1
@@ -0,0 +1,39 @@
+﻿# # echo "Hello world!"
+
+# $array = 1..10
+
+# $xml_info = [xml](Get-Content '.\sample.xml')
+
+# # Write-Host($xml_info)
+
+# foreach ($x in $xml_info) {
+#     Write-Host "$x"
+# }
+
+# # foreach ($i in $array) {
+# #     Write-Host($i)
+# # }
+
+# 実行中のパス取得/移動
+$path = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $path
+
+# ファイル書きこみ
+# StreamReaderのコンストラクタに直接 「$path + "\test.txt"」を入力するとエラーになるので分ける
+$array = 1..10
+foreach ($i in 1..1) {
+    $fileName = $path + "\test$i.hoge"
+    $file = New-Object System.IO.StreamWriter($fileName, $false, [System.Text.Encoding]::GetEncoding("unicode"))
+    foreach ($j in 1..1000) {
+        $file.WriteLine("あいうえお")
+    }
+    $file.WriteLine("かきくけこ")
+    $file.WriteLine("さしすせそ")
+    # Write-Host("")
+    $file.Close()
+}
+
+# calc # 電卓起動
+
+# 終了
+Write-Host("終了")
